de2e0dedbe9ee1733425992cdef16f2bf1946600
Shamreen Tabassum <shamreen1tabassumzj@gmail.com>
2021-04-01 14:06:57 +0530

    Update Generate random password.py

--- Generate random password.py
+++ Generate random password.py
@@ -1,3 +1,5 @@
+To create a password with Python, we need to create a program that takes the length of the password and generates a random password of the same length.
+
 # Random password generator
 
 import string
